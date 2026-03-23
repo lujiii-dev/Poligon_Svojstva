875c022472b67bfe3ecc06466494b42d08840c4a
Ucenik <Ucenik@DESKTOP-A02KV2H>
2026-03-23 18:46:01 +0100

    v2.1

---
+++ Poligon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygon___Properties
+{
+    internal class Poligon
+    {
+    }
+}
